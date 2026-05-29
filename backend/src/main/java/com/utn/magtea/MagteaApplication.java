605bbc389bc7cd6413239401e1cc30bd0137f46a
Ignacio Marucco <ignaciomarucco@gmail.com>
2026-05-29 16:23:57 -0300

    Configuracion inicial del proyecto

---
+++ backend/src/main/java/com/utn/magtea/MagteaApplication.java
@@ -0,0 +1,13 @@
+package com.utn.magtea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagteaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagteaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/utn/magtea/MagteaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.magtea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagteaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
